ecc4f281e803d400b5354de475fc02c39f586cb2
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-05-28 21:20:12 +0530

    feat: create new modules

---
+++ src/app/pins/pins.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PinComponent } from './pin/pin.component';
+
+
+
+@NgModule({
+  declarations: [
+    PinComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PinsModule { }
